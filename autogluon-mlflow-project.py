f85fbbefd2016c201d55462b7b81cf176a16d5a6
MaHoFai <47138261+mahofai@users.noreply.github.com>
2023-07-19 11:42:06 +0800

    Update autogluon-mlflow-project.py

--- autogluon-mlflow-project.py
+++ autogluon-mlflow-project.py
@@ -26,6 +26,7 @@ train_feature_generator = PipelineFeatureGenerator(
             IdentityFeatureGenerator(infer_features_in_args=dict(
                 valid_raw_types=[R_INT, R_FLOAT])),
         ],
+        
      ],
     verbosity=3,
     post_drop_duplicates=False,
@@ -54,11 +55,19 @@ class AutogluonModel(mlflow.pyfunc.PythonModel):
 
 
 if __name__ == "__main__":
-    one_hot_train_data = TabularDataset('train.csv')
-    one_hot_test_data = TabularDataset('test.csv')
-    one_hot_train_data = one_hot_train_data.iloc[:500]
-    one_hot_test_data = one_hot_test_data.iloc[:100]
-    concatenated_df  = pd.concat([one_hot_train_data,one_hot_test_data], axis=0)
+    train_data = TabularDataset('train.csv')
+    test_data = TabularDataset('test.csv')
+    train_data = train_data
+    test_data = test_data
+    concatenated_df  = pd.concat([train_data,test_data], axis=0)
+    
+    alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
+    # time_limit = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
+    quality = sys.argv[2] 
+    print(" !!!!!quality:", quality)
+    print("alpha",alpha)
+    # print("time_limit",time_limit)
+    time_limit=1000000
     
     train_feature_generator = PipelineFeatureGenerator(
         generators=[
@@ -66,12 +75,16 @@ if __name__ == "__main__":
                 IdentityFeatureGenerator(infer_features_in_args=dict(
                     valid_raw_types=[R_INT, R_FLOAT])),
             ],
+            
         ],
         verbosity=3,
         post_drop_duplicates=False,
         post_generators=[IdentityFeatureGenerator()]
     )
-    one_hot_train_data = train_feature_generator.fit_transform(X=concatenated_df)
+    one_hot_all_data = train_feature_generator.fit_transform(X=concatenated_df)
+    
+    one_hot_train_data = one_hot_all_data[:len(train_data)]
+    one_hot_test_data = one_hot_all_data[len(train_data):]
     
     # print(concatenated_df)
     one_hot_valid_data1 = one_hot_train_data[one_hot_train_data["fold"] ==0.0]
@@ -80,36 +93,22 @@ if __name__ == "__main__":
     one_hot_train_data1 = one_hot_train_data1.drop(["fold"],axis=1)
     one_hot_valid_data1 = one_hot_valid_data1.drop(["fold"],axis=1)
     
-    alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
-    l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
-    print("alphaP:",alpha)
-    print("l1_ratio:",l1_ratio)
-
-    print("one_hot_train_data1.shape",one_hot_train_data1.shape)
+    # print("one_hot_train_data1.shape",one_hot_train_data1.shape)
+    # print("one_hot_valid_data1.shape",one_hot_valid_data1.shape)
     # print(one_hot_valid_data1)
 
     with mlflow.start_run() as run:
-
+        
         predictor = TabularPredictor(label='solubility',eval_metric="precision")
-        predictor.fit(train_data=one_hot_train_data1, tuning_data=one_hot_valid_data1, feature_generator=None)
+        if quality == "medium_quality":
+            predictor.fit(train_data=one_hot_train_data1, tuning_data=one_hot_valid_data1, feature_generator=None, time_limit=time_limit,presets=quality)
+        elif quality == "best_quality":
+            predictor.fit(train_data=one_hot_train_data1, feature_generator=None, time_limit=time_limit,presets=quality)
+        evaluation = predictor.evaluate(one_hot_test_data, silent=True)
+        print("test eval:",evaluation)
+        
+        mlflow.log_metric("precision", evaluation["precision"])
+        mlflow.log_metric("auc", evaluation["roc_auc"])
+        mlflow.log_metric("mcc", evaluation["precision"])
 
-        predictor.leaderboard(one_hot_test_data, silent=True)
-        
-        model = AutogluonModel(predictor)
-        
-        model_info = mlflow.pyfunc.log_model(
-            artifact_path='ag-model', python_model=model
-        )
-        
-        loaded_model = mlflow.pyfunc.load_model(model_uri=model_info.model_uri).unwrap_python_model()
-
-        
-        ## evaluate mode demo
-        val_metrics = loaded_model.evaluate(one_hot_valid_data1)
-        test_metrics = loaded_model.evaluate(one_hot_test_data)
-        for k,v in val_metrics.items():
-            log_metric('valid_'+k, v)
-        for k,v in test_metrics.items():
-            log_metric('test_'+k, v)
-            
         mlflow.end_run()
