82e4c91226c9faaa71fce3196a3aa653c44b0b6c
mahofai <u3556527@connect.hku>
2023-10-11 17:25:47 +0800

    add hyperparameter for random search

--- autoMM.py
+++ autoMM.py
@@ -29,10 +29,6 @@ from sklearn.model_selection import train_test_split
 
 from zipfile import ZipFile
 import os
-
-'''
-data format: one csv file, split data with "split" column (train/test/valid)
-'''
  
 parser = argparse.ArgumentParser(description='argument parser')
 # complusory settings
@@ -197,7 +193,7 @@ if __name__ == "__main__" :
     
     num_trails = args.num_trials
     hyperparameter_tune_kwargs = {}
-    
+    custom_hyperparameters={}
     if args.mode == "manual":
         print("manual !!!")
         if args.searcher == "grid":
@@ -228,6 +224,16 @@ if __name__ == "__main__" :
             hyperparameter_tune_kwargs["searcher"] = "random"
             hyperparameter_tune_kwargs["scheduler"] = "ASHA"
             hyperparameter_tune_kwargs["num_trials"] = args.num_trials
+            custom_hyperparameters={
+                "optimization.learning_rate": tune.uniform(args.lr[0], args.lr[-1]),
+                "optimization.lr_decay":tune.uniform(args.lr_decay[0], args.lr_decay[-1]),
+                "optimization.weight_decay": tune.uniform(args.lr_decay[0], args.lr_decay[-1]),
+                "env.batch_size": tune.choice(args.batch_size),
+                "optimization.optim_type": tune.choice(args.optim_type),
+                'model.hf_text.checkpoint_name': args.check_point_name,
+                'optimization.max_epochs': args.max_epochs,
+                "optimization.lr_schedule":tune.choice(args.lr_schedule),
+            }
 
         else:
             print("no searcher. skip hpo")
